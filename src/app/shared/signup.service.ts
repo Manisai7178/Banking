335da72d27fcee95dc2e06880b065fd8d29731f0
Manisai7178 <Manisai.Patha@lntinfotech.com>
2020-01-27 18:03:17 +0530

    secondcommit

--- src/app/shared/signup.service.ts
+++ src/app/shared/signup.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { Router } from 'onlinebanking/node_modules/@angular/router/router';
+import {Router} from '@angular/router';
 @Injectable({
 
   providedIn: 'root'
